0a12fd70141d9b115d4351b8a0465b3df8532e40
Kola Ra <santaramus@gmail.com>
2019-10-10 17:19:44 +0300

    add excersise4

---
+++ src/main/java/excersise4/Result.java
@@ -0,0 +1,15 @@
+package excersise4;
+
+public class Result {
+    public static void main(String[] args) {
+        float firstOperation = -5 + 8 * 6;
+        System.out.println("-5 + 8 * 6" + " = " + (int)firstOperation);
+        float secondOperation = (55+9) % 9;
+        System.out.println("(55+9) % 9 " + " = " + (int)secondOperation);
+        float thirdOperation =  20 + -3*5 / 8;
+        System.out.println("20 + -3*5 / 8" + " = " + (int)thirdOperation);
+        float fourthOperation = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println("5 + 15 / 3 * 2 - 8 % 3" + " = " + (int)fourthOperation);
+
+    }
+}
