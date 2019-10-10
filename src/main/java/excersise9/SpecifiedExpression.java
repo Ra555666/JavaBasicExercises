214985f3db99134e3069dbc03217e3ac1413c5f6
Kola Ra <santaramus@gmail.com>
2019-10-10 19:57:42 +0300

    add true excersise9

---
+++ src/main/java/excersise9/SpecifiedExpression.java
@@ -0,0 +1,8 @@
+package excersise9;
+
+public class SpecifiedExpression {
+    public static void main(String[] args) {
+        double specifiedExpression = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(specifiedExpression);
+    }
+}
